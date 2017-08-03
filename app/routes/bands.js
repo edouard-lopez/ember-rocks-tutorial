e5993100a1d25977b80b82a2ce16155fb7637ddc
Édouard Lopez <edouard.lopez@people-doc.com>
2017-08-03 17:38:18 +0200

    add songs & bands models

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
+var Band = Ember.Object.extend({
+  name: ''
+});
+
 export default Ember.Route.extend({
+  model: function () {
+    var ledZeppelin = { name: 'Led Zeppelin' };
+    var pearlJam = { name: 'Pearl Jam' };
+    var fooFighters = { name: 'Foo Fighters' };
+
+    return [ledZeppelin, pearlJam, fooFighters].map(band => Band.create(band));
+  }
 });
--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
-
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
 export default Ember.Route.extend({
+  model: function() {
+    var blackDog = { title: 'Black Dog', band: 'Led Zeppelin', rating: 3 };
+    var yellowLedbetter = { title: 'Yellow Ledbetter', band: 'Pearl Jam', rating: 4 };
+    var pretender = { title: 'The Pretender', band: 'Foo Fighters', rating: 2 };
+
+    return [blackDog, yellowLedbetter, pretender].map(song => Song.create(song));
+  }
 });
